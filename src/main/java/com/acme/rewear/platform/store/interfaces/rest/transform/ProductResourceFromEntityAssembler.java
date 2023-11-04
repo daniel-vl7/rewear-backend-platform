195f86be49c43570afe8729809339483f0652c0a
daniel <u202121440@upc.edu.pe>
2023-11-04 16:42:17 -0500

    feat(products): added product resource from entity

---
+++ src/main/java/com/acme/rewear/platform/store/interfaces/rest/transform/ProductResourceFromEntityAssembler.java
@@ -0,0 +1,10 @@
+package com.acme.rewear.platform.store.interfaces.rest.transform;
+
+import com.acme.rewear.platform.store.domain.model.aggregates.Product;
+import com.acme.rewear.platform.store.interfaces.rest.resources.ProductResource;
+
+public class ProductResourceFromEntityAssembler {
+    public static ProductResource toResourceFromEntity(Product entity){
+        return new ProductResource(entity.getId(), entity.getProductName(), entity.getProductDescription(), entity.getProductDetail());
+    }
+}
