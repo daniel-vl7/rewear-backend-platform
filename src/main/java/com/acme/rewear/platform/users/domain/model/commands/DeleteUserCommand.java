79d4a4466b116c107bb41439c7df2997059b928b
Adrian Melgar <DaikiMT@outlook.es>
2023-11-23 17:34:33 -0500

    Removed old users BC and added extra configuration in application.properties

--- src/main/java/com/acme/rewear/platform/users/domain/model/commands/DeleteUserCommand.java
+++
@@ -1,4 +0,0 @@
-package com.acme.rewear.platform.users.domain.model.commands;
-
-public record DeleteUserCommand(Long id) {
-}
--- src/main/java/com/acme/rewear/platform/users/interfaces/rest/resources/LoginUserResource.java
+++
@@ -1,4 +0,0 @@
-package com.acme.rewear.platform.users.interfaces.rest.resources;
-
-public record LoginUserResource(String username, String password) {
-}
