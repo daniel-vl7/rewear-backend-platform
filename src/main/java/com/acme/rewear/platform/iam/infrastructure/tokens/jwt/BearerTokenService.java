d2512b5532a1d65443ea112e65cb35ac38f386a0
Adrian Melgar <DaikiMT@outlook.es>
2023-11-23 16:51:16 -0500

    Added application.internal file layout

--- src/main/java/com/acme/rewear/platform/iam/infrastructure/tokens/jwt/BearerTokenService.java
+++ src/main/java/com/acme/rewear/platform/iam/infrastructure/tokens/jwt/BearerTokenService.java
@@ -1,4 +1,30 @@
 package com.acme.rewear.platform.iam.infrastructure.tokens.jwt;
 
-public interface BearerTokenService {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+
+/**
+ * BearerTokenService
+ * <p>
+ *     Service to handle bearer token.
+ *     This service is used to generate and validate bearer token.
+ *     This service is used to extract bearer token from request.
+ * </p>
+ */
+public interface BearerTokenService extends TokenService {
+
+    /**
+     * Extract bearer token from request.
+     * @param request the http request
+     * @return bearer token
+     */
+    String getBearerTokenFrom(HttpServletRequest request);
+
+    /**
+     * Generate bearer token from authentication.
+     * @param authentication the authentication object
+     * @return bearer token
+     */
+    String generateToken(Authentication authentication);
 }
