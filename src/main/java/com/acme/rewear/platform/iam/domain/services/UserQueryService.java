5656da6bc04c14136bf5d603172ec79742502f4a
Adrian Melgar <DaikiMT@outlook.es>
2023-11-23 16:28:20 -0500

    Implemented IAM's domain

---
+++ src/main/java/com/acme/rewear/platform/iam/domain/services/UserQueryService.java
@@ -0,0 +1,29 @@
+package com.acme.rewear.platform.iam.domain.services;
+
+import com.acme.rewear.platform.iam.domain.model.aggregates.User;
+import com.acme.rewear.platform.iam.domain.model.queries.GetAllUsersQuery;
+import com.acme.rewear.platform.iam.domain.model.queries.GetUserByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * UserQueryService
+ * <p>
+ *     Service to handle user queries.
+ * </p>
+ */
+public interface UserQueryService {
+
+    /**
+     * Handle the get all users query.
+     * <p>
+     *     This method is responsible for retrieving all the users from the database.
+     * </p>
+     * @param query The get all users query.
+     * @return The list of users.
+     * @see GetAllUsersQuery
+     */
+    List<User> handle(GetAllUsersQuery query);
+    Optional<User> handle(GetUserByIdQuery query);
+}
