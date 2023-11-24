4c097715c9c344b6fd1dfc8ded5cba97fd11ed48
Adrian Melgar <DaikiMT@outlook.es>
2023-11-24 12:25:17 -0500

    Added get alll and get by id queries for event

---
+++ src/main/java/com/acme/rewear/platform/store/domain/model/queries/GetAllEventsQuery.java
@@ -0,0 +1,4 @@
+package com.acme.rewear.platform.store.domain.model.queries;
+
+public record GetAllEventsQuery() {
+}
---
+++ src/main/java/com/acme/rewear/platform/store/domain/model/queries/GetEventByIdQuery.java
@@ -0,0 +1,4 @@
+package com.acme.rewear.platform.store.domain.model.queries;
+
+public record GetEventByIdQuery(Long eventId) {
+}
